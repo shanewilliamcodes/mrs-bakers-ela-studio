c4741eaab40acdcaa492c3a81a7d4eaeae76e888
Shane <shanewillb@gmail.com>
2026-06-10 16:45:16 -0400

    Connect Firebase authentication and secure bellwork data

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,8 @@
-window.BAKER_FIREBASE_CONFIG = null;
+window.BAKER_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDvzE1Jagk4wCGdk-5SdBNno_Zjal0DFnk",
+  authDomain: "mrs-baker-s-ela-studio.firebaseapp.com",
+  projectId: "mrs-baker-s-ela-studio",
+  storageBucket: "mrs-baker-s-ela-studio.firebasestorage.app",
+  messagingSenderId: "650441830838",
+  appId: "1:650441830838:web:56bab484ed3e5b5c89d117"
+};
